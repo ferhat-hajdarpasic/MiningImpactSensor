759d23d7bcd69e6f8cd61da487fe7fc35c0128a3
Ferhat Hajdarpasic <ferhat.hajdarpasic@live.com.au>
2016-10-29 13:11:43 +1100

    Main screen now automatically shows all availabe devices and connect to them for reading movement.

--- Windows/Pages/DevicePage.xaml.cs
+++ Windows/Pages/DevicePage.xaml.cs
@@ -14,146 +14,15 @@ namespace MiningImpactSensor.Pages
 {
     public sealed partial class DevicePage : Page
     {
-        DispatcherTimer _timer;
         SensorTag sensor;
-        bool registeredConnectionEvents;
-        ObservableCollection<TileModel> tiles = new ObservableCollection<TileModel>();
-
         public DevicePage()
         {
             this.InitializeComponent();
-
-            Clear();
-
-        }
-
-        protected async override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            sensor = e.Parameter as SensorTag;
-
-            SensorList.ItemsSource = tiles;
-
-            sensor.MovementDataChanged += OnMovementMeasurementValueChanged;
-
-            Boolean success = await sensor.ConnectMotionService();
-
-            if(success)
-            {
-                AddTile(new TileModel() { Caption = "Accelerometer", Icon = new BitmapImage(new Uri("ms-appx:/Assets/Accelerometer.png")) });
-            }
-
-            base.OnNavigatedTo(e);
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
             base.OnNavigatedFrom(e);
-        }
-
-        void OnStatusChanged(object sender, string status)
-        {
-            DisplayMessage(status);
-        }
-
-        double Fahrenheit(double celcius)
-        {
-            return celcius * 1.8 + 32.0;
-        }
-
-
-        string FormatTemperature(double t)
-        {
-            if (!Settings.Instance.Celcius)
-            {
-                t = Fahrenheit(t);
-            }
-            return t.ToString("N2");
-        }
-
-        private void OnMovementMeasurementValueChanged(object sender, SensorTag.MovementDataChangedEventArgs e)
-        {
-            var nowait = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, new Windows.UI.Core.DispatchedHandler(() =>
-            {
-                string caption = Math.Round(e.X, 3) + "," + Math.Round(e.Y, 3) + "," + Math.Round(e.Z, 3);
-                var a = GetTile("Accelerometer");
-                if (a != null)
-                {
-                    a.SensorValue = caption;
-                }
-            }));
-        }
-
-        private TileModel GetTile(string name)
-        {
-            return (from t in tiles where t.Caption == name select t).FirstOrDefault();
-        }
-
-        void OnServiceError(object sender, string message)
-        {
-            DisplayMessage(message);
-        }
-
-        private void Clear()
-        {
-            foreach (var tile in tiles)
-            {
-                tile.SensorValue = "";
-            }
-        }
-
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.  The Parameter
-        /// property is typically used to configure the page.</param>
-        public async void Show()
-        {
-        }
-
-        private void DisplayMessage(string message)
-        {
-            var nowait = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, new Windows.UI.Core.DispatchedHandler(() =>
-            {
-                ErrorMessage.Text = message;
-            }));
-        }
-
-        private void OnItemClick(object sender, ItemClickEventArgs e)
-        {
-            TileModel tile = e.ClickedItem as TileModel;
-            if (tile != null)
-            {
-                SelectTile(tile);
-            }
-        }
-
-        private void SelectTile(TileModel model)
-        {
-            Frame frame = Window.Current.Content as Frame;
-
-            switch (model.Caption)
-            {
-                case "Accelerometer":
-                    frame.Navigate(typeof(AccelerometerPage));
-                    break;
-            }
-        }
-
-
-        private void AddTile(TileModel model)
-        {
-            if (!(from t in tiles where t.Caption == model.Caption select t).Any())
-            {
-                tiles.Add(model);
-            }
-        }
-
-        private void RemoveTiles(IEnumerable<TileModel> enumerable)
-        {
-            foreach (TileModel tile in enumerable.ToArray())
-            {
-                tiles.Remove(tile);
-            }
         }
 
         private void OnGoBack(object sender, RoutedEventArgs e)
