f9db9e4a04189bcef43e621b1798e4047287e04f
Ferhat Hajdarpasic <ferhat.hajdarpasic@live.com.au>
2016-11-26 13:24:51 +1100

    Renamed deployment artifacts to 'shokpod'.

--- Windows/Properties/AssemblyInfo.cs
+++ Windows/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MiningImpactSensor")]
+[assembly: AssemblyTitle("Shokpod")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MiningImpactSensor")]
+[assembly: AssemblyProduct("Shokpod")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
